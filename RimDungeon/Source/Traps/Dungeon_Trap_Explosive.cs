3237ca6bcf49c126474d1c463dad3653199b7f79
Mikolaj Grobelny <miki4920@gmail.com>
2020-11-21 22:23:35 +0000

    Added Oil trap and texture, separated turrets into separate files
    TODO: Figure out ammunition changing for turrets, and arc of fire.

---
+++ RimDungeon/Source/Traps/Dungeon_Trap_Explosive.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Rimdungeon.Traps
+{
+	public class Building_Trap_Explosive : Dungeon_Trap_Framework
+	{
+		protected override void SpringSub(Pawn p)
+		{
+			base.GetComp<CompExplosive>().StartWick(null);
+		}
+	}
+}
+
---
+++ RimDungeon/Source/Turrets/Dungeon_Turret_Framework.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rimdungeon.Turrets
+{
+    class Dungeon_Turret_Framework
+    {
+    }
+}
